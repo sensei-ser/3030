7c67b6c74af5e0f54aa6991dcf582b925c4176c6
sensei-ser <ser123russ@gmail.com>
2025-10-14 08:21:15 +0300

    Create group-kick.js

---
+++ plugins/group-kick.js
@@ -0,0 +1,19 @@
+let handler = async (m, { conn, participants, usedPrefix, command, isROwner }) => {
+	
+let kickte = `🚩 Укажите пользователя, которого вы хотите снести.`
+
+if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte)}) 
+let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
+let owr = m.chat.split`-`[0]
+await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
+m.reply(`🚩 Удаленный пользователь.`)
+m.reply(`Извините, вас только что исключили из группы.`, user)
+}
+
+handler.help = ['kick *@user*']
+handler.tags = ['group']
+handler.command = ['снести', 'expulsar'] 
+handler.group = true
+handler.botAdmin = true
+
+export default handler
