352b2e89939c8d692ae8919ea64af2412d0bd664
sensei-ser <ser123russ@gmail.com>
2025-10-16 10:58:35 +0300

    Update nv-global-1.js

--- plugins/nv-global-1.js
+++ plugins/nv-global-1.js
@@ -236,6 +236,11 @@ let vn = './media/бухаю.mp4'
 this.sendPresenceUpdate('recording', m.chat)   
 this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
 
+if (!chat.isBanned && chat.audios && m.text.match(/(приветствие|новенький)/gi)) {    
+let vn = './media/приветствие.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+
 
 
 
